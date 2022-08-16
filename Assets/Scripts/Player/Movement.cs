95f7aa579d46a84523a74dc263b46c5a62ab3a22
COBASilver <xarashocoba@gmail.com>
2022-08-16 11:58:57 +0300

    Jump, ground check.
    Сделай прыжок что-бы он был удобным (Ну хз можешь из референсов платформеры или подобную хуйню).

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-	Vector2 moveVector;
+	public Transform GroundCheck;
 	private Rigidbody2D rb;
+    Vector2 moveVector;
 
-	private bool canFlip;
-	public float speed = 10.0f;
-    public float jumpForce = 5f;
+    public float speed, jumpForce, time_jump, check_radius; 
+	public LayerMask ground;
+	public bool is_ground;
+
+    private bool canFlip, is_jumping;
+    private float time_jump_true;
 
     void Start()
 	{
@@ -18,13 +22,15 @@ public class Movement : MonoBehaviour
 
 	void Update()
 	{
-		moveVector.x = Input.GetAxis("Horizontal");
-		Jump();
-		if (moveVector.x > 0 && canFlip)
+        Jump();
+		if (moveVector.x > 0 && canFlip || moveVector.x < 0 && !canFlip)
 			Flip();
-		if (moveVector.x < 0 && !canFlip)
-			Flip();
-        rb.velocity = new Vector2(moveVector.x * speed, rb.velocity.y);
+    }
+
+	void FixedUpdate()
+	{
+        //Иначе он когда только начинает прыгать останавливается и только когда на половине прыжка начинает двигаться
+        Walk();
     }
 
 	void Flip()
@@ -33,9 +39,35 @@ public class Movement : MonoBehaviour
 		transform.Rotate(0f, 180f, 0f);
 	}
 
+	void Walk()
+	{
+        moveVector.x = Input.GetAxis("Horizontal");
+		rb.velocity = new Vector2(moveVector.x * speed, rb.velocity.y);
+    }
+
 	void Jump()
 	{
-		if (Input.GetKeyDown(KeyCode.Space))
-			rb.AddForce(Vector2.up * jumpForce);
-	}
+		is_ground = Physics2D.OverlapCircle(GroundCheck.position, check_radius, ground);
+
+		if (Input.GetKeyDown(KeyCode.Space) && is_ground)
+		{
+			is_jumping = true;
+			time_jump_true = time_jump;
+			rb.velocity = Vector2.up * jumpForce;
+		}
+		if (Input.GetKey(KeyCode.Space) && is_jumping)
+		{
+			if (time_jump_true >= 0)
+			{
+				rb.velocity = Vector2.up * jumpForce;
+				time_jump_true -= Time.deltaTime;
+			}
+			else
+				is_jumping = false;
+		}
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			is_jumping = false;
+		}
+    }
 }
